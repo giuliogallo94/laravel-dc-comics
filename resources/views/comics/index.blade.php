4a0183b93f12297c8b587710cf4247d45c3d3b23
giuliogallo94 <giuliogallo1994@gmail.com>
2024-01-19 10:38:17 +0100

    add: index table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,13 +1,28 @@
 @extends('layout.app') 
 
 @section('content') 
-        @foreach ($comics as $comic)
-            <div>
-                <ul>
-                    <li>
-                        {{ $comic->title}}
-                    </li>
-                </ul>
-            </div>
-@endforeach
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Title</th>
+                <th scope="col">Series</th>
+                <th scope="col">Type</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)   
+                <tr>
+                    <th scope="row">{{$comic->id}}</th>
+                    <td>{{$comic->title}}</td>
+                    <td>{{$comic->series}}</td>
+                    <td>{{$comic->type}}</td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection 
