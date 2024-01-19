7ee6ad781c892c9922a9dc14b5aec9c9c379b6d8
giuliogallo94 <giuliogallo1994@gmail.com>
2024-01-19 10:04:26 +0100

    add: fillable function

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -16,17 +16,20 @@ class ComicsTableSeeder extends Seeder
     public function run()
     {
         $comic_list = config('comics');
-        // dd($comic_list);
+        
         foreach ($comic_list as $comic) {
             $new_comic = new Comic();
-            $new_comic->title = $comic['title'];
-            $new_comic->description = $comic['description'];
-            $new_comic->thumb = $comic['thumb'];
-            $new_comic->price = $comic['price'];
-            $new_comic->series = $comic['series'];
-            $new_comic->sale_date = $comic['sale_date'];
-            $new_comic->type = $comic['type'];
+            // $new_comic->title = $comic['title'];
+            // $new_comic->description = $comic['description'];
+            // $new_comic->thumb = $comic['thumb'];
+            // $new_comic->price = $comic['price'];
+            // $new_comic->series = $comic['series'];
+            // $new_comic->sale_date = $comic['sale_date'];
+            // $new_comic->type = $comic['type'];
 
+            $new_comic->fill($comic);
+
+            // dd($comic_list);
             $new_comic->save();
         }
 
